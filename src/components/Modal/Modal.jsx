c4689e605027df399e7b3e249efa5a2fee5e4034
John Guilding <jwguilding@gmail.com>
2020-11-26 22:14:23 +0000

    modal styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -18,6 +18,11 @@ const Modal = (props) => {
   // IF MODAL CLICKED SHOW MODAL //
   const modalShow = modal ? styles.showModal : styles.hideModal;
 
+  // const foodPairingsLi = food_pairing ? food_pairing.forEach(pair => (
+  //   <li>{pair}</li>
+  // )) : (<li>pairs</li>);
+  // console.log(food_pairing);
+
   const closeModal = () => {
     setModal(!modal);
   }
@@ -25,19 +30,26 @@ const Modal = (props) => {
   return (
     <section className={`${styles.modal} ${modalShow}`}>
       <FontAwesomeIcon icon={'fa', 'times'} onClick={closeModal} className={styles.closeModal} />
+
       <article className={styles.card}>
+
         <div className={styles.container}>
           <div className={styles.beerImg}>
             <div className={styles.circle}></div>
             <img src={image_url} alt="A picture of beer"/>
           </div>
-          <div className={styles.info}>
+          <div className={styles.infoColumn1}>
             <h2 className={styles.title}>{name}</h2>
             <h3>{tagline}</h3>
+            <p>{abv}</p>
+          </div>
+          <div className={styles.infoColumn2}>
             <p>{description}</p>
           </div>
         </div>
+
       </article>
+
     </section>
   );
 };
